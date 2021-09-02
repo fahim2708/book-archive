7136f3cf0dfe39ddf7a7ba794166c2ad9ba69de2
thedevfahim <fahimulislam.official@gmail.com>
2021-09-02 22:21:47 +0600

    final

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const searchBook = () => {
     const getSearchInput = document.getElementById('search-field');
     const searchInput = getSearchInput.value;
+
     //console.log(searchInput);
     getSearchInput.value = '';
     const url = `https://openlibrary.org/search.json?q=${searchInput}`;
@@ -10,31 +11,35 @@ const searchBook = () => {
         .then(res => res.json())
         .then(data => displaySearchResult(data.docs))
 }
-
 //Display Search Result
 const displaySearchResult = docs => {
     const searchResult = document.getElementById('search-result');
+    // Show total search result
+    const totalSearch = document.getElementById('total-search');
+    const total = docs.length;
+    if (total > 0) {
+        totalSearch.innerText = `Total ${total} books found `;
+    }
+    else {
+        totalSearch.innerHTML = `<span class="text-danger">No Result found</span>`;
+    }
     //clear previous search result
     searchResult.textContent = "";
     docs.forEach(doc => {
-        //console.log(doc);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div class="card h-100 bg-dark border border-secondar">
         <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top h-75 w-50 mx-auto mt-3">
             <div class="card-body text-center">
-                <h4 class="card-title"><b> ${doc.title} </b></h4>
-                <p class="card-text text-secondary"><i> ${doc.author_name?.[0]} </i></p>
-                <p class="card-text"><b>Publisher: </b> ${doc.publisher?.[0]}</p>
-                <p class="card-text"><b>First Publish Year: </b> ${doc.first_publish_year}</p>
+                <h4 class="card-title text-white"><b> ${doc.title} </b></h4>
+                <p class="card-text text-secondary"><i>By ${doc.author_name?.[0]} </i></p>
+                <p class="card-text text-white"><b>Publisher: </b> ${doc.publisher?.[0]}</p>
+                <p class="card-text text-white"><b>First Publish Year: </b> ${doc.first_publish_year}</p>
+                <button class="btn btn-success text-white fw-bold"><i class="fas fa-download">      Download</i></button>
             </div>
         </div>`;
         searchResult.appendChild(div);
-
-
-
-
     });
 
 }
